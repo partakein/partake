b8d7524d57caa913df5b8feed4c664749823dd08
MAYAH <mayah@mayah.jp>
2012-07-07 19:32:01 +0900

    partake-all.min.js and partake-all.min.css should have a QueryString to disable user cache when a server is updated.
    This patch adds QueryString to partake-all.min.js and partake-all.min.css.
    
    Fixed #308

--- app/in/partake/app/PartakeApp.java
+++ app/in/partake/app/PartakeApp.java
@@ -1,11 +1,15 @@
 package in.partake.app;
 
+import in.partake.base.DateTime;
+import in.partake.base.TimeUtil;
 import in.partake.service.IDBService;
 import in.partake.service.IEventSearchService;
 import in.partake.service.IOpenIDService;
 import in.partake.service.ITwitterService;
 
 public class PartakeApp {
+    public static final DateTime initializedAt = TimeUtil.getCurrentDateTime();
+
     protected static IDBService dbService;
     protected static IEventSearchService eventSearchService;
     protected static ITwitterService twitterService;
